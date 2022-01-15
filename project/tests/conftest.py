c8d3392fa867427c8d1865121bb78a2057e8545c
Vitor Hugo <vitorhugoserravalle@gmail.com>
2022-01-15 18:35:56 -0300

    new test for invalid json

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -5,6 +5,7 @@ from starlette.testclient import TestClient
 
 from app.main import create_application
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
