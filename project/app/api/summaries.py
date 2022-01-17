222cac9d6a0a471727eac5ef5468d260d5d489e8
Vitor Hugo <vitorhugoserravalle@gmail.com>
2022-01-16 21:51:38 -0300

    feat: add test_read_summary

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+from app.models.tortoise import SummarySchema
 
 
 router = APIRouter()
@@ -19,3 +20,10 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         "url": payload.url
     }
     return response_object
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+
+    return summary
+
