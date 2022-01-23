f766b1a6399e7397818bd09359fe5b54d93df1e9
Vitor Hugo <vitorhugoserravalle@gmail.com>
2022-01-23 17:05:59 -0300

    feat: add put route

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -3,7 +3,7 @@
 
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema
+from app.models.pydantic import SummaryPayloadSchema, SummaryUpdatePayloadSchema
 from app.models.tortoise import TextSummary
 
 
@@ -26,3 +26,15 @@ async def get(id: int) -> Union[dict, None]:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(url=payload.url, summary=payload.summary)
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
