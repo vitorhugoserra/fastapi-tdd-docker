d3a3f7add471cddef5d4908ee784a08de2636e28
Vitor Hugo <vitorhugoserravalle@gmail.com>
2022-01-24 09:10:48 -0300

    feat: passed flake8, black and isort in the code.

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -3,7 +3,8 @@
 
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema, SummaryUpdatePayloadSchema
+from app.models.pydantic import (SummaryPayloadSchema,
+                                 SummaryUpdatePayloadSchema)
 from app.models.tortoise import TextSummary
 
 
@@ -27,13 +28,16 @@ async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
 
+
 async def delete(id: int) -> int:
     summary = await TextSummary.filter(id=id).first().delete()
     return summary
 
 
 async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
-    summary = await TextSummary.filter(id=id).update(url=payload.url, summary=payload.summary)
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
     if summary:
         updated_summary = await TextSummary.filter(id=id).first().values()
         return updated_summary
