6aefa2e98e9a2226c4df78e6391aedefeaa2acdc
Vitor Hugo <vitorhugoserravalle@gmail.com>
2022-01-23 21:35:33 -0300

    feat: updated "update" tests

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -124,6 +124,23 @@ def test_update_summary_incorrect_id(test_app_with_db):
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
+    response = test_app_with_db.put(
+        f"/summaries/0/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["path", "id"],
+                "msg": "ensure this value is greater than 0",
+                "type": "value_error.number.not_gt",
+                "ctx": {"limit_value": 0},
+            }
+        ]
+    }
+
+
 def test_update_summary_invalid_json(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -172,5 +189,9 @@ def test_update_summary_invalid_keys(test_app_with_db):
         ]
     }
 
-
-
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+    )
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
